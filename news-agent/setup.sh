7b0b03b1ae0fb9fb603c875fde2db4aa4b0f9538
Anirudh Kamble <connect.anirudh@gmail.com>
2026-06-25 18:34:31 +0530

    Add setup.sh for agents

---
+++ news-agent/setup.sh
@@ -0,0 +1,10 @@
+echo "Creating virtual environment..."
+python3 -m venv .venv
+
+echo "Activating virtual environment..."
+source .venv/bin/activate
+
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
